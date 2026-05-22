c74cf5ee0045ceefe64b6479336d124f90de9f32
Jory <moxspell@msn.com>
2026-05-22 09:26:12 +0200

    Fix Sanity config inline env vars

--- sanity.config.js
+++ sanity.config.js
@@ -13,13 +13,10 @@ import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
